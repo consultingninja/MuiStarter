b8fba41fddb28de570b5e2dfce7e79d60ba8ee08
Consulting Ninja <116985458+consultingninja@users.noreply.github.com>
2022-12-01 16:30:54 -0600

    Add files via upload
    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import MiniDrawer from './MiniDrawer';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <MiniDrawer />
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import mui from 'vite-plugin-material-ui'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),mui]
+})
